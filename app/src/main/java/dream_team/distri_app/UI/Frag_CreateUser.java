2d2a51d06a90555a71dd539ef7014c0679948b7e
Martin Vieth <martin_123_vieth@hotmail.com>
2016-04-05 12:49:51 +0200

    lille fix, kom til at fjerne dependency

--- app/src/main/java/dream_team/distri_app/UI/Frag_CreateUser.java
+++ app/src/main/java/dream_team/distri_app/UI/Frag_CreateUser.java
@@ -3,11 +3,18 @@ package dream_team.distri_app.UI;
 import android.os.Bundle;
 
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.URL;
+import java.net.URLConnection;
 
 import dream_team.distri_app.R;
 
@@ -16,6 +23,7 @@ public class Frag_CreateUser extends Fragment implements View.OnClickListener {
 
     Button btnSubmit;
     EditText edtUsername, edtPassword1,edtPassword2;
+    Integer doubledValue =0;
 
     public Frag_CreateUser() {
         // Required empty public constructor
@@ -40,14 +48,53 @@ public class Frag_CreateUser extends Fragment implements View.OnClickListener {
 
 
     public void onClick(View v) {
-        if (v ==btnSubmit) {
+        if (v == btnSubmit) {
 
-            //do Stuff
+            new Thread(new Runnable() {
+                public void run() {
+
+                    try {
+                        URL url = new URL("http://10.0.2.2:8080/MyServletProject/DoubleMeServlet");
+                        URLConnection connection = url.openConnection();
+
+                        String inputString = edtUsername.getText().toString();
+                        //inputString = URLEncoder.encode(inputString, "UTF-8");
+
+                        Log.d("inputString", inputString);
+
+                        connection.setDoOutput(true);
+                        OutputStreamWriter out = new OutputStreamWriter(connection.getOutputStream());
+                        out.write(inputString);
+                        out.close();
+
+                        BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+
+                        String returnString = "";
+                        doubledValue = 0;
+
+                        while ((returnString = in.readLine()) != null) {
+                            doubledValue = Integer.parseInt(returnString);
+                        }
+                        in.close();
+
+                        /*
+                        runOnUiThread(new Runnable() {
+                            public void run() {
+
+                                edtUsername.setText(doubledValue.toString());
+
+                            }
+                        });
+                        */
+
+                    } catch (Exception e) {
+                        Log.d("Exception", e.toString());
+                    }
+
+                }
+            }).start();
+
+
         }
-
     }
-
-
-
-
 }
